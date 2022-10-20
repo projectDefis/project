2ecdbf6b29de742f2c517a916cd9933959b5159a
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-20 15:47:15 +0200

    first commit branch dev-section2

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    // branch Axel
+    // branch Gorkem
     <div className="bg-custom-gray-100">
       <h1 className="font-inter">Hello project</h1>
       <h1 className="font-roboto">Hello project</h1>
