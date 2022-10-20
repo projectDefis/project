f218bac7da901a32a929e9b418d6b1b70503dbef
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-20 15:28:39 +0200

    Update home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="bg-custom-gray-100">
+      <h1 className="font-inter">Hello project</h1>
+      <h1 className="font-roboto">Hello project</h1>
+    </div>
+  );
+}
+
+export default Home;
