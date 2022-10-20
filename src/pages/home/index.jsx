a46d35de6b58330e59b0ba1c74fa69b78a5f1242
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-20 15:50:08 +0200

    update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,5 @@
 const Home = () => {
   return (
-    // branch Enrick
     <div className="bg-custom-gray-100">
       <h1 className="font-inter">Hello project</h1>
       <h1 className="font-roboto">Hello project</h1>
