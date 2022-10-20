e0f09fdf8e4a314eeacf9ead305d25fcd1571731
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-20 15:23:05 +0200

    add custom fonts and colors

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter"],
+      roboto: ["Roboto"],
+    },
+    extend: {
+      colors: {
+        "custom-gray-100": "#BFBFBF",
+        "custom-gray-200": "#ADB5BD",
+        "custom-gray-300": "#828282",
+        "custom-gray-700": "#232323",
+        "custom-gray-900": "#767E90",
+        "custom-black-100": "#E5E5E5",
+        "custom-black-500": "#202020",
+        "custom-brown": "#2C2C2C",
+        "custom-orange": "#F87F28",
+        "custom-red": "#BB2323",
+      },
+    },
+  },
+  plugins: [],
+};
