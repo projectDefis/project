4fc3b09c7f193ef9dc1dc796a0b2f69bd3d5e6af
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-26 09:56:01 +0200

    add CSS for news

---
+++ src/components/news/styles.jsx
@@ -0,0 +1,22 @@
+// CSS news section7
+
+export const styles = {
+  // section for principal div
+  divSectionContent: "bg-custom-black-500 text-white font-inter pl-7",
+
+  // border orange
+  borderOrange: "pt-5 pb-10 pl-0.5",
+
+  //   font & size & etc
+
+  text: {
+    h2: "text-4xl  font-semibold leading-extra-height",
+    h3: "text-lg font-bold leading-6 pb-2.5 pt-2.5 pl-2 w-72",
+    paragraphItalic: "text-xl  italic ",
+    paragraph2: "text-sm font-normal leading-6 pt-2.5 pb-8 pl-2 w-52",
+    paragraphYoutube: "text-sm font-normal leading-6 pl-2 pb-14",
+    spanDate:
+      "text-xs uppercase text-custom-orange tracking-widest font-roboto font-bold pl-2",
+    uppercase: "uppercase",
+  },
+};
